7a411020e8a7d354e6cab17af096c13b18ad50d9
Yurii Matora <yura.matora11@gmail.com>
2020-05-15 01:26:17 +0300

    Prepare project structure for MVVM

--- app/src/main/java/com/pumpit/app/ui/view/activity/MainActivity.java
+++ app/src/main/java/com/pumpit/app/ui/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.pumpit.app;
+package com.pumpit.app.ui.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pumpit.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
