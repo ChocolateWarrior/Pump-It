4aa31ee63dfd9a4b18adf363f99f94d0ad0f13a8
ChocolateWarrior <markzubach13@gmail.com>
2020-06-03 20:23:35 +0300

    Changed main activity to home activity added previous main activity and its layout

--- app/src/main/java/com/pumpit/app/ui/view/activity/MainActivity.java
+++ app/src/main/java/com/pumpit/app/ui/view/activity/MainActivity.java
@@ -1,34 +1,30 @@
 package com.pumpit.app.ui.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.pumpit.app.R;
+import com.pumpit.app.ui.view.activity.registration.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawer;
+    private Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+
+        test = findViewById(R.id.test);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
 }
